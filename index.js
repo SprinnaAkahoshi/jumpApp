3143e4aa743b2f26929dced30d71b9317376ea6d
sakahoshi <sprinnaa@zillowgroup.com>
2020-08-01 22:19:31 -0700

    created landing page and summary page

---
+++ index.js
@@ -0,0 +1,14 @@
+var submitButton = document.querySelector("#submit-button");
+
+
+const submit = () => {
+    console.log(69*69);
+}
+
+submitButton.addEventListener("click", submit);
+
+
+const sprinna = "female";
+const kevin = "male";
+const dexter = "cat";
+console.log(sprinna);
